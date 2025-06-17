4d1f3b031e6ad9fc2b6829a27cf243d02573d792
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-17 13:52:29 -0300

    att

--- src/session/constructor.lua
+++ src/session/constructor.lua
@@ -1,8 +1,6 @@
 Private.newSession  = function(request_provider)
     local selfobj = {}
-    selfobj.request_provider = request_provider 
-
+    selfobj.request_provider = request_provider
     Private.create_print_function(selfobj)
-    
     return selfobj
-end 
+end
