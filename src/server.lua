75912414097ba78b962a764578b9a57ff71af761
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-09 18:51:10 -0300

    att

--- src/server.lua
+++ src/server.lua
@@ -4,7 +4,7 @@
 
 WebDriver.newLocalServer = function(props)
 
-    if not props.chromedriver_comand then
+    if not props.chromedriver_command then
         error("chromedriver_command is required")
     end
     if not props.chrome_binary then
@@ -15,18 +15,26 @@ WebDriver.newLocalServer = function(props)
         port = props.port or 4444,
     }
 
-    function turnof_chromedriver()
-        props.fetch({
-            url="127.0.1:"..selfobj.port.."/shutdown",
-            method="DELETE",
-        })
-
+    function turnoff_chromedriver()
+        if props.fetch then
+            props.fetch({
+                url="http://127.0.0.1:"..selfobj.port.."/shutdown",
+                method="DELETE",
+            })
+        end
     end
-    setmetatable(selfobj,{__gc = turnof_chromedriver})
-    local command = "%s --port=%d --binary=%s & "
-    command = command:format(props.chromedriver_comand, selfobj.port, props.chrome_binary)
+    
+    setmetatable(selfobj,{__gc = turnoff_chromedriver})
+    
+    -- Start chromedriver with proper command formatting
+    local command = "%s --port=%d --binary=%s &"
+    command = command:format(props.chromedriver_command, selfobj.port, props.chrome_binary)
+    
+    print("Starting chromedriver with command: " .. command)
     os.execute(command)
-    --sleep for 1 second to allow the server to start
-    os.execute("sleep 1")
+    
+    -- Wait for chromedriver to start
+    os.execute("sleep 2")
+    
     return selfobj
 end
