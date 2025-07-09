0bb35123065cd89aec5cf75a640b791156a32d90
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-09 20:41:27 -0300

    att

--- src/session.lua
+++ src/session.lua
@@ -1,1 +1,18 @@
 
+Private.newSession = function (props)
+    local result = props.fetch({
+        url = string.format(props.url .."/session"),
+        method = "POST",
+        body ={
+            capabilities = {
+                alwaysMatch = {
+                    browserName = "chrome",
+                 
+                }
+            }
+        }
+    })
+    print(result.status_code)
+
+
+end
