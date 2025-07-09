39cadc88e45685e00f3f68fd60a127505e7bb9ff
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-09 18:31:47 -0300

    att

--- src/types.lua
+++ src/types.lua
@@ -2,7 +2,7 @@
 ---@class WebDriverServer
 
 ---@class WebDriver
----@field newLocalServer fun(fetch:function,chromedriver_path: string,chrome_comand:string): WebDriverServer
+---@field newLocalServer fun(props:table): WebDriverServer
 
 ---@type WebDriver
 WebDriver = WebDriver 
