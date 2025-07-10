104e3e32d825840a02cc80a81bcf78c9db2bb75f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-10 02:11:45 -0300

    att

--- src/session.lua
+++ src/session.lua
@@ -1,8 +1,23 @@
 
+
+Session.__gc = function (public, private)
+    print("Closing session with ID: " .. private.session_id)
+    local result = private.props.fetch({
+        url = private.props.url .. "/session/" .. private.session_id,
+        method = "DELETE",
+        http_version = "1.1"
+    })
+    if result.status_code ~= 200 then
+        print("Failed to close session: " .. result.read_body())
+    end
+end
+
+
 Session.newSession = function (props)
 
     local selfobject = Heregitage.newMetaObject()
     selfobject.private_props_extends(props)
+    selfobject.set_meta_method("__gc", Session.__gc)
     
     local result = props.fetch({
         url=props.url.."/session",
@@ -19,8 +34,11 @@ Session.newSession = function (props)
             }
         }
     })
-    print(result.status_code)
-    print(result.read_body())
+    local body = result.read_body_json()
+    selfobject.private.session_id = body.value.sessionId
+    
+
+    
     return selfobject.public
 
 end
