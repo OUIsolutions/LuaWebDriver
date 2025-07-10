28c93fdbddd1ee086e56605c6fde1d4698edd91e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-10 00:30:59 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,10 +1,19 @@
+
+os.execute("mkdir -p dependencies")
+if not darwin.dtw.isfile("dependencies/herigitage.lua") then 
+  os.execute("curl -L https://github.com/mateusmoutinho/LuaHeritage/releases/download/1.0.0/heregitage.lua -o dependencies/herigitage.lua")
+end
+local heregitage = darwin.dtw.load_file("dependencies/herigitage.lua")
+
 local all = {
   [[return (function()    
       local Private = {};
       local WebDriver = {};
       local Server  ={}
   ]]
-    }
+  }
+  all[#all + 1] = "local Heregitage = (function()  "..heregitage .. " end\n)()\n"
+
   local files = darwin.dtw.list_files_recursively("src",true)
 
   for _, file in ipairs(files) do
