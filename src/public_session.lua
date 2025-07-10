c862306b7d8f399560172b28ebfad7e281998258
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-10 03:05:37 -0300

    att

--- src/public_session.lua
+++ src/public_session.lua
@@ -1,5 +1,16 @@
 
 PublicSession.navegate_to = function(public,private,url)
 
-    
+    --make a requisiton to navegate_to a url
+    local result = private.fetch({
+        url = private.url .. "/session/" .. private.session_id .. "/url",
+        method = "POST",
+        http_version = "1.1",
+        body = {
+            url = url
+        }
+    })
+    if result.status_code ~= 200 then
+        error("Failed to navigate to URL: " .. result.read_body())
+    end
 end
