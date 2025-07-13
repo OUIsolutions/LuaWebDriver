84e564d7eb5a5fbc78c310727efc1df823d98942
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-13 00:15:29 -0300

    added get text

--- src/element/retrivers.lua
+++ src/element/retrivers.lua
@@ -1,4 +1,3 @@
--- Função para obter o HTML do elemento
 PublicElement.get_html = function(public, private)
     local response = private.fetch({
         method = "GET",
@@ -15,7 +14,28 @@ PublicElement.get_html = function(public, private)
     return nil
 end
 
--- Função para obter um atributo específico do elemento
+
+
+
+PublicElement.get_text = function(public, private)
+    local response = private.fetch({
+        method = "GET",
+        http_version = "1.1",
+        url = private.url .. "/session/" .. private.session_id .. "/element/" .. private.element_id .. "/text"
+    })
+    
+    if response.status_code == 200 then
+        local body = response.read_body_json()
+        if body and body.value then
+            return body.value
+        end
+    end
+    return nil
+end
+
+
+
+
 PublicElement.get_attribute = function(public, private, attribute_name)
     if not attribute_name or type(attribute_name) ~= "string" then
         error("Nome do atributo deve ser uma string não vazia")
