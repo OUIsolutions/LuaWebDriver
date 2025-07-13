993e06f9286bca610a442a640161ee53ba124c93
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-12 22:49:43 -0300

    at

--- src/element/constructor.lua
+++ src/element/constructor.lua
@@ -1,7 +1,12 @@
+Element.__tostring = function (public,private)
+    return public.get_html()
+end
+
 
 Element.newElement = function (props)
     local selfobject = Heregitage.newMetaObject()
     selfobject.private_props_extends(props)
     selfobject.public_method_extends(PublicElement)
+    selfobject.set_meta_method("__tostring", Element.__tostring)
     return selfobject.public
 end 
