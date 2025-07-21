bdedc7e6577a6262959c42e62a475e45480a2421
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-21 17:47:42 -0300

    att

--- src/session/extra.lua
+++ src/session/extra.lua
@@ -1,3 +1,15 @@
 PublicSession.get_session_id = function(public, private)
     return private.session_id
 end
+
+PublicSession.accept_alert = function(public, private)
+    local result = private.fetch({
+        url = private.url .. "/session/" .. private.session_id .. "/alert/accept",
+        method = "POST",
+        http_version = "1.1"
+    })
+    
+    if result.status_code ~= 200 then
+        error("Failed to accept alert: " .. result.read_body())
+    end
+end
