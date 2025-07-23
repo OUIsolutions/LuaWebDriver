883d18722f5e079610b89250a955f69240f52994
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-23 14:17:31 -0300

    added args

--- src/server.lua
+++ src/server.lua
@@ -15,7 +15,7 @@ Server.newSession = function(public,private, props)
         error("binary_location is required")
     end
 
-    return Session.newSession({url = private.url, fetch = private.fetch,binary_location= props.binary_location})
+    return Session.newSession({url = private.url, fetch = private.fetch,binary_location= props.binary_location,args=props.args})
 end
 
 
